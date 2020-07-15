e29e6aa3f307b6f04bb04073b28b00e32cc9958e
ayainokuchi <r_aya_o.729@hotmail.co.jp>
2020-07-15 12:21:38 +0000

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charaset="utf-8">
+    <title>Minimal Akita</title>
+    <link href="css/home.css" rel="stylesheet">
+</head>
+<body>
+<h1>Minimal Akita -ミニマルアキタ-</h1>
+<p>行きたいを見つける行きつけを見つける小さなお店の情報シェアサイトです。</p>
+
+
+
+</body>
+</html>
